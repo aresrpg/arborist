f2dbea5d59707b655c4b4a139e6e6c414a9d346e
Duarte David <deltaduartedavid@gmail.com>
2021-02-17 00:05:31 +0100

    Add Sidebar and Toolbar

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tree class="tree" :root="tree" />
+  <main class="w-full flex flex-row">
+    <Sidebar
+      class="w-2/12"
+      :items="trees.map(({ name }) => name)"
+      :selected="selectedTree"
+      @select="(i) => (selectedTree = i)"
+    />
+    <div class="w-full flex flex-col">
+      <Toolbar :instances="trees[selectedTree].instances" />
+      <Tree
+        class="w-full h-full tree bg-green-700"
+        :root="tree"
+        @select="select"
+        :selected="selected"
+      />
+    </div>
+  </main>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue'
+import equal from 'fast-deep-equal'
+
 import Tree from '../components/Tree.vue'
+import Sidebar from '../components/Sidebar.vue'
+import Toolbar from '../components/Toolbar.vue'
 
 const trees = await fetch('http://127.0.0.1:4242/behavior').then((res) =>
   res.json()
@@ -18,16 +39,30 @@ function parseNode(node) {
   }
 }
 
-const tree = parseNode(
-  new DOMParser().parseFromString(trees[0].tree, 'text/xml').documentElement
-)
+let selectedTree = ref(0)
+
+const tree = computed(() => ({
+  name: `Root (${trees[selectedTree.value].id})`,
+  children: [
+    parseNode(
+      new DOMParser().parseFromString(
+        trees[selectedTree.value].tree,
+        'text/xml'
+      ).documentElement
+    ),
+  ],
+}))
+
+let selected = ref(null)
+
+function select(path) {
+  if (equal(selected.value, path)) selected.value = null
+  else selected.value = path
+}
 </script>
 
 <style scoped>
 .tree {
-  width: 100%;
-  height: 100%;
-  background-color: #269;
   background-image: linear-gradient(
       rgba(255, 255, 255, 0.5) 2px,
       transparent 2px
