67cc85b3102b6703dfd1172969b88cc80277989c
Duarte David <deltaduartedavid@gmail.com>
2021-02-21 21:52:27 +0100

    Add router and remove hardcoded api url

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Editor from './views/Editor.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: `/${encodeURIComponent('http://localhost:4242/behavior/')}`,
+  },
+  {
+    path: '/:url/:tree?',
+    component: Editor,
+  },
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+})
